65a1ceed6ecca21e04d265d85a078421a784d0ec
Iurii Razubaiev <yr.fine@gmail.com>
2017-05-14 09:44:34 +0300

    Generated yeoman project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name videoFeedExerciseApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the videoFeedExerciseApp
+ */
+angular.module('videoFeedExerciseApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name videoFeedExerciseApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the videoFeedExerciseApp
+ */
+angular.module('videoFeedExerciseApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });

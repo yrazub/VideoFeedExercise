18b02eb50091f0ee5a370480485cd7b2131d9b6a
Iurii Razubaiev <iurii.razubaiev@perfectial.com>
2017-05-15 17:54:15 +0300

    Basic Video feed

--- app/scripts/modules/video/video.module.js
+++ app/scripts/modules/video/video.module.js
@@ -1,3 +1,13 @@
 'use strict';
 
-angular.module('video', []);
+angular.module('video', [])
+    .factory('config', function(){
+        return {
+            videoFeedUrl: 'https://cdn.playbuzz.com/content/feed/items'
+        };    
+    })
+    
+    .factory('VideoFeed', ['$resource', 'config', function($resource, config){
+        return $resource(config.videoFeedUrl);
+    }]);
+
--- app/scripts/modules/video/videoFeed.controller.js
+++ app/scripts/modules/video/videoFeed.controller.js
@@ -1,6 +1,21 @@
 'use strict';
 
 angular.module('video')
-  .controller('VideoFeedCtrl', function () {
+  .controller('VideoFeedCtrl', ['$scope', 'VideoFeed', '$timeout', function ($scope, VideoFeed, $timeout) {
+      $scope.items = [];
+      $scope.loading = true;
 
-  });
+      function load(){
+          let p = VideoFeed.get(function(response){
+            $scope.items = response.items;
+          }).$promise;
+
+          p.finally(function(){
+            $timeout(function(){
+              $scope.loading = false;
+            }, 1000);
+          });
+      }
+
+      load();
+  }]);
